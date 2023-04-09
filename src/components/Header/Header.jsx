b308219d236fb672914f17c295666b77ca17731e
Kateryna Magur <64228839+KaterynaMagur@users.noreply.github.com>
2023-04-09 21:32:30 +0300

    added secondary style for the logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,15 @@
 import { Button } from '../../common/Button/Button';
 import { Logo } from './components/Logo/Logo';
+
 import styles from './test.module.scss';
+
+const NickName = 'Kate';
 
 export const Header = () => {
 	return (
 		<div className={styles.header}>
 			<Logo primary />
-			<div className={styles.nick}>Kate</div>
+			<div className={styles.nick}>{NickName}</div>
 			<Button primary>Logout</Button>
 		</div>
 	);
