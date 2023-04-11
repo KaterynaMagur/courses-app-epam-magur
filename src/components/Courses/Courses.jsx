8b616c8c650c84b276a43ff09fd31ed976817931
Kateryna Magur <64228839+KaterynaMagur@users.noreply.github.com>
2023-04-11 19:59:54 +0300

    added search by title and id

--- src/components/Courses/Courses.jsx
+++ src/components/Courses/Courses.jsx
@@ -3,17 +3,27 @@ import { CourseCard } from './components/CourseCard/CourseCard';
 import { Button } from '../../common/Button/Button';
 import styles from './Courses.module.scss';
 import { SearchBar } from './components/SearchBar/SearchBar';
+import { useState } from 'react';
 
 export const Courses = () => {
+	const [search, setSearch] = useState('');
+
 	return (
 		<div className={styles.mainContent}>
 			<div className={styles.controlDiv}>
-				<SearchBar />
+				<SearchBar onSearch={setSearch} />
 				<Button primary>Add new course</Button>
 			</div>
-			{mockedCoursesList.map((course) => (
-				<CourseCard course={course} key={course.id} />
-			))}
+			{mockedCoursesList
+				.filter(
+					(course) =>
+						course.title.toLowerCase().search(search) !== -1 ||
+						course.id.search(search) !== -1
+				)
+
+				.map((course) => (
+					<CourseCard course={course} key={course.id} />
+				))}
 		</div>
 	);
 };
