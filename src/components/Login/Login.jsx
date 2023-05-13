bb68795cfeda2c2243b22ca0067364ec109a67eb
Kateryna Magur <64228839+KaterynaMagur@users.noreply.github.com>
2023-05-13 19:33:59 +0300

    added some styles to new components

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,13 +1,16 @@
 import { Input } from '../../common/Input/Input';
 import { Button } from '../../common/Button/Button';
+import styles from './Login.module.scss';
 
 export const Login = () => {
 	return (
-		<div>
-			<h2>Login</h2>
+		<div className={styles.card}>
+			<h2 className={styles.title}>Login</h2>
 			<Input labelText='Email' placeholderText='Enter email'></Input>
 			<Input labelText='Password' placeholderText='Enter password'></Input>
-			<Button primary>Login</Button>
+			<div className={styles.button}>
+				<Button primary>Login</Button>
+			</div>
 			<div>
 				If you not have an account you can<a>Registration</a>
 			</div>
--- src/components/Registration/Registration.jsx
+++ src/components/Registration/Registration.jsx
@@ -1,16 +1,20 @@
 import { Input } from '../../common/Input/Input';
 import { Button } from '../../common/Button/Button';
 
+import styles from './Registration.module.scss';
+
 export const Registration = () => {
 	return (
-		<div>
-			<h2>Registration</h2>
+		<div className={styles.card}>
+			<h2 className={styles.center}>Registration</h2>
 			<Input labelText='Name' placeholderText='Enter name'></Input>
 			<Input labelText='Email' placeholderText='Enter email'></Input>
 			<Input labelText='Password' placeholderText='Enter password'></Input>
-			<Button primary>Registration</Button>
-			<div>
-				If you an account you can <a>Login</a>
+			<div className={styles.center}>
+				<Button primary>Registration</Button>
+			</div>
+			<div className={styles.center}>
+				If you have an account you can <a>Login</a>
 			</div>
 		</div>
 	);
