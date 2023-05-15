a33339a1a62b96154e622f68b367f19120a0468f
Kateryna Magur <64228839+KaterynaMagur@users.noreply.github.com>
2023-05-15 15:40:16 +0300

    added structure, link for the CourseInfo component

--- src/components/CourseInfo/CourseInfo.jsx
+++ src/components/CourseInfo/CourseInfo.jsx
@@ -1,16 +1,31 @@
-import { Button } from '../../common/Button/Button';
+import { Link, useParams } from 'react-router-dom';
+import { mockedAuthorsList, mockedCoursesList } from '../../constants';
+import { useMemo } from 'react';
 
 export const CourseInfo = () => {
+	const { courseId } = useParams();
+
+	const course = useMemo(() => {
+		return mockedCoursesList.find((course) => course.id === courseId);
+	}, [courseId]);
+
+	const authors = useMemo(() => {
+		return mockedAuthorsList
+			.filter((author) => course.authors.includes(author.id))
+			.map((author) => author.name)
+			.join(', ');
+	}, [course]);
+
 	return (
 		<div>
-			<Button primary> &larr; Back to courses</Button>
-			<h2>Course Name</h2>
-			<div>Course info</div>
+			<Link to='/courses'> &larr; Back to courses</Link>
+			<h2>{course?.title}</h2>
+			<div>{course?.description}</div>
 			<div>
-				<div>ID: </div>
-				<div>Duration: </div>
-				<div>Created: </div>
-				<div>Authors: </div>
+				<div>{course?.id}</div>
+				<div>{course?.duration}</div>
+				<div>{course?.creationDate}</div>
+				<div>{authors}</div>
 			</div>
 		</div>
 	);
