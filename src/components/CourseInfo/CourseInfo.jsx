b33b3f8ea69222c51ec75dac9652753400501af9
Kateryna Magur <64228839+KaterynaMagur@users.noreply.github.com>
2023-05-16 11:40:45 +0300

    added styles for CourseInfo

--- src/components/CourseInfo/CourseInfo.jsx
+++ src/components/CourseInfo/CourseInfo.jsx
@@ -1,6 +1,10 @@
 import { Link, useParams } from 'react-router-dom';
 import { mockedAuthorsList, mockedCoursesList } from '../../constants';
 import { useMemo } from 'react';
+import styles from './CourseInfo.module.scss';
+import { padTo2Digits } from '../../helpers/pipeDuration';
+
+const MINUTES_ONE_HOUR = 60;
 
 export const CourseInfo = () => {
 	const { courseId } = useParams();
@@ -17,15 +21,28 @@ export const CourseInfo = () => {
 	}, [course]);
 
 	return (
-		<div>
+		<div className={styles.card}>
 			<Link to='/courses'> &larr; Back to courses</Link>
-			<h2>{course?.title}</h2>
-			<div>{course?.description}</div>
-			<div>
-				<div>{course?.id}</div>
-				<div>{course?.duration}</div>
-				<div>{course?.creationDate}</div>
-				<div>{authors}</div>
+			<h2 className={styles.title}>{course?.title}</h2>
+			<div className={styles.infoPart}>
+				<div>{course?.description}</div>
+				<div className={styles.rightPart}>
+					<div>
+						<span className={styles.label}>ID:</span> {course?.id}
+					</div>
+					<div>
+						<span className={styles.label}>Duration: </span>
+						{padTo2Digits(Math.floor(course?.duration / MINUTES_ONE_HOUR))}:
+						{padTo2Digits(course?.duration % MINUTES_ONE_HOUR)} hours
+					</div>
+					<div>
+						<span className={styles.label}>Created:</span>{' '}
+						{course?.creationDate}
+					</div>
+					<div>
+						<span className={styles.label}>Authors:</span> {authors}
+					</div>
+				</div>
 			</div>
 		</div>
 	);
